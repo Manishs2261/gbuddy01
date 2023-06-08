9fb5422679572b33606f2847419968ac90037640
Manish sahu <sahusanju138@gmail.com>
2023-06-08 22:16:57 +0530

    second comite

--- app/src/main/java/com/bilaspur/manishsahu/ggu/gubuddy01/MainActivity.java
+++ app/src/main/java/com/bilaspur/manishsahu/ggu/gubuddy01/MainActivity.java
@@ -2,7 +2,9 @@ package com.bilaspur.manishsahu.ggu.gubuddy01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,25 +12,25 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button= (Button) findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
+            public void run() {
 
-                FirebaseDatabase.getInstance().getReference().child("head").setValue("Hello");
-
-
+                startActivity(new Intent(MainActivity.this,LoginPage.class));
+                finishAffinity();
 
             }
-        });
+        },1000);
+
+
+
     }
 
 
---
+++ app/src/main/java/com/bilaspur/manishsahu/ggu/gubuddy01/loginclass/Student_login.java
@@ -0,0 +1,16 @@
+package com.bilaspur.manishsahu.ggu.gubuddy01.loginclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bilaspur.manishsahu.ggu.gubuddy01.R;
+
+public class Student_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+    }
+}
